a9a763aa14e735adc6e1fb112ef8123443a975db
Abr98 <abr98@nau.edu>
2017-01-31 11:50:39 -0700

    Create BlocksViewer.java

---
+++ BlocksViewer.java
@@ -0,0 +1,15 @@
+import javax.swing.Jframe;
+import java.awt.Color;
+
+public class BlocksViewer {
+  public static void main(String[] args) {
+    JFrame frame = new JFrame();
+    frame.setSize(10*20, 26*20);
+    frame.setTitle("Tretis Snapshot");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setBackground(Color.GRAY);
+    // Create a new component that knows how to draw blocks
+    // Add that component to the JFrame
+    // Make the frame visible so things render
+  }
+}
