1052e8438532222436db11b63d930520c22cba47
caleyhalpern <cmh695@nau.edu>
2017-02-06 18:57:17 -0700

    Update BlocksComponent.java

--- BlocksComponent.java
+++ BlocksComponent.java
@@ -2,19 +2,28 @@ import java.awt.Graphics;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
-import java.awt.swing.JComponent;
+import javax.swing.JComponent;
+
 class BlocksComponent extends JComponent {
   
-  @Override
+ // @Override
   protected void paintComponent(Graphics g) {
     Graphics2D g2 = (Graphics2D) g;    
     
-    g2.setColor(Color.CYAN);
     Rectangle block_t1 = new Rectangle(20, 0, 20, 80);
-    g2.draw(block_t1);
+    Rectangle block_t2 = new Rectangle(0, 0, 20, 40);
+    Rectangle block_t3 = new Rectangle(0,0, 20, 60);
+    
+    Color cyan = new Color(CYAN);
+    Color red = new Color(RED);
+    Color yellow = new Color(YELLOW);
+    Color green = new Color(GREEN);
+    Color purple = new Color(MAGENTA);
+    Color blue = new Color(BLUE);
+    
+    g2.translate(20, 40);
+    g2.setPaint(cyan);
     g2.fill(block_t1);
-    
-    
       
       
   }
