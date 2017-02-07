1f1b8593c0ae90d53965f384551efda5794d9048
yc357 <yc357@nau.edu>
2017-02-07 05:07:18 -0700

    Update BlocksComponent.java

--- BlocksComponent.java
+++ BlocksComponent.java
@@ -11,21 +11,43 @@ public class BlocksComponent extends JComponent {
     // recover Graphics2D
     Graphics2D g2 = (Graphics2D) g;    
     
+    // draw 7 different rectangles
+    g2.setColor(Color.CYAN);
     Rectangle block_I = new Rectangle(0, 0, 20, 80);
-    Rectangle block_J = new Rectangle(0, 0, 20, 40);
-    Rectangle block_L = new Rectangle(0, 0, 20, 60);
-    Rectangle block_O = new Rectangle(0, 0, 20, 60);
-    Rectangle block_S = new Rectangle(0, 0, 20, 60);
-    Rectangle block_Z = new Rectangle(0, 0, 20, 60);
-    Rectangle block_T = new Rectangle(0, 0, 20, 60);
+    g2.fill(block_I);
     
+    g2.setColor(Color.BLUE);
+    Rectangle block_J = new Rectangle(0, 0, 20, 40);
+    g2.fill(block_J);
+    
+    g2.setColor(Color.ORANGE);
+    Rectangle block_L = new Rectangle(0, 0, 20, 60);
+    g2.fill(block_L);
+    
+    g2.setColor(Color.YELLOW);
+    Rectangle block_O = new Rectangle(0, 0, 20, 60);
+    g2.fill(block_O);
+    
+    g2.setColor(Color.GREEN);
+    Rectangle block_S = new Rectangle(0, 0, 20, 60);
+    g2.fill(block_S);
+    
+    g2.setColor(Color.RED);
+    Rectangle block_Z = new Rectangle(0, 0, 20, 60);
+    g2.fill(block_Z);
+    
+    g2.setColor(Color.RED);
+    Rectangle block_T = new Rectangle(0, 0, 20, 60);
+    g2.fill(block_T);
+    
+    /*
     Color cyan = new Color(CYAN);
     Color red = new Color(RED);
     Color yellow = new Color(YELLOW);
     Color green = new Color(GREEN);
     Color purple = new Color(PURPLE);
     Color blue = new Color(BLUE);
-    Color orange = new Color(orange);
+    Color orange = new Color(orange);*/
     
     g2.translate(20, 40);
     g2.setPaint(cyan);
