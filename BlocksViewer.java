a370a37af77d96de45e7f9272a76219243585783
yc357 <yc357@nau.edu>
2017-02-07 08:01:33 -0700

    Update BlocksViewer.java

--- BlocksViewer.java
+++ BlocksViewer.java
@@ -4,7 +4,7 @@ import java.awt.Color;
 public class BlocksViewer {
   public static void main(String[] args) {
     JFrame frame = new JFrame();
-    frame.setSize(10*20, 26*20);
+    frame.setSize(11*20, 26*20);
     frame.setTitle("Tretis Snapshot");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setBackground(Color.GRAY);
